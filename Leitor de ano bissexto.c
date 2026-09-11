f83dea53519c9450f25bc7d8878836c983ee7055
GusGus194 <bernardogustavo71@gmail.com>
2026-09-11 18:36:00 -0300

    Leitor de ano bissexto.c

--- Leitor de ano bissexto.c
+++ Leitor de ano bissexto.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Faça um programa que leia um ano (valor inteiro) e imprima se ele é bissexto ou não. OBS:
+/*Um programa que le um ano (valor inteiro) e imprima se ele é bissexto ou não. OBS:
 São bissexto todos os anos múltiplos de 400. Não sendo múltiplo de 400, são bissextos todos
 os anos múltiplos de 4 mas que não são múltiplos de 100.*/
 
