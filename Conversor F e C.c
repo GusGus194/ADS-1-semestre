cc6e2bc34c591720ee52e6bbe57cc668f677c03d
GusGus194 <bernardogustavo71@gmail.com>
2026-09-11 18:29:50 -0300

    Update and rename main.c to Conversor F e C.c

--- Conversor F e C.c
+++ Conversor F e C.c
@@ -2,20 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*Faça um programa que lê um caracter ’F’ ou ’C’, que indica se o próximo número a ser
-digitado corresponde a temperatura em Fahrenheit ou Celsius. Em seguida o programa
-deve ler o valor da temperatura e então imprimir o valor correspondente da temperatura
-na outra unidade de medida. Obs.: (C = 5/9 · (F 32)).*/
+// O programa lê um caracter ’f’ ou ’c’ e indica o equivalente na outra escala de medida de temperatura. (C = 5/9 · (F 32)).
 
 int main()
 {   double num1, cel, far;
-    char G;
+    char Graus;
 
     printf("Bem vindo ao conversor de graus C e F,\n primeiramente, escolha qual escala de medida quer começar: \n");
-    scanf ("%c", &G);
+    scanf ("%c", &Graus);
 
 //C para F
-    if (G == 'c') {
+    if (Graus == 'c' || Graus == 'C') {
         printf("Certo, quantos graus Celsius?");
         scanf ("%lf", &num1);
         far = (num1 * 9 / 5) + 32;
@@ -23,7 +20,7 @@ int main()
         printf("\nPerfeito. %.2lf em Celsius é igual a %.2lf em Fahrenheit!", num1, far);
     }
 //F para C
-    else if (G == 'f') {
+    else if (Graus == 'f' || Graus == 'F') {
        printf("Certo, quantos graus Celsius?");
         scanf ("%lf", &num1);
         cel = (num1 - 32) * 5 / 9;
